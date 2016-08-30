f05d57769399c330c862a3d57b090f554a9423c4
garianandroid <garianandroidgames@gmail.com>
2016-08-31 03:54:21 +0530

    added day-1/Print.py

---
+++ journey/day-1/Print.py
@@ -0,0 +1,39 @@
+################################ Print.py ##################################
+# desc   : code that describes print command in 8 useful scenarios         #
+# author : @rishabhio                                                      #
+# website: https://rishabh.io                                              #
+# repo   : https://github.com/rishabhio/PythonCave                         #
+# created: 31-08-16:02:19 ( DD-MM-YY:HH:MM )                               #
+############################################################################
+
+# print a simple greeting 
+print 'Hello Uncle Print, I am very happy to be here'
+
+# print with a newline escape character 
+print '\nHello Uncle Print. \nI am very happy to be here'
+
+# print with a apostrophe escape character
+print '\nI\'m so happy to be in Python\'s cave'
+
+# print strings in concatenation
+print '\n%s %s %s' %('I','am','combining strings')
+
+# print strings with integers 
+print '\n%s %d' %('remember everything starts with a ', 0)
+
+# print strings with floats 
+print '\n%s %f %s' %('value of pi i.e.', 3.14, 'is just another scientific opinion')
+
+# print numbers using a specific length by padding space in front
+print '\n%s %4d' %('the number with a length of 4 is', 25)
+
+# print floats to a certain decimal places 
+print '\n%s %4.2f' %('float with length 4 printed to 2 decimal places: ', 2.5051995)
+
+# print signed numbers 'negative numbers'
+print '\n% d' % (- 122)
+
+# print signed numbers 'positive numbers'
+print '\n%+d' % (14)
+
+
